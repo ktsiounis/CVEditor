8147f7ead1947ebb03bc1681c6ec89c92e4a75b8
ktsiounis <ntinos.tsi199@gmail.com>
2017-04-04 22:11:41 +0300

    Deleted some useless code

--- src/Tests/CombinedCVControllerTest.java
+++ src/Tests/CombinedCVControllerTest.java
@@ -1,6 +1,5 @@
 import controller.CombinedCVController;
 import javafx.application.Application;
-import javafx.stage.Stage;
 import model.CV;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
--- src/model/CV.java
+++ src/model/CV.java
@@ -1,8 +1,6 @@
 package model;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
